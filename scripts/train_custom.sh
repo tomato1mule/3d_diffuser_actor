5ebd20724a4673a2a7ac0d65c6e487090ad3792f
Hyunwoo Ryu <tomato1mule@gmail.com>
2024-03-15 14:45:53 +0000

    cuda_launch_blocking=0 to avoid error

--- scripts/train_custom.sh
+++ scripts/train_custom.sh
@@ -10,7 +10,7 @@ B=3
 C=120
 ngpus=1
 
-CUDA_LAUNCH_BLOCKING=1 torchrun --nproc_per_node $ngpus --master_port $RANDOM \
+CUDA_LAUNCH_BLOCKING=0 torchrun --nproc_per_node $ngpus --master_port $RANDOM \
     main_keypose.py \
     --tasks place_cups close_jar insert_onto_square_peg light_bulb_in meat_off_grill open_drawer place_shape_in_shape_sorter place_wine_at_rack_location push_buttons put_groceries_in_cupboard put_item_in_drawer put_money_in_safe reach_and_drag slide_block_to_color_target stack_blocks stack_cups sweep_to_dustpan_of_size turn_tap \
     --dataset $dataset \
